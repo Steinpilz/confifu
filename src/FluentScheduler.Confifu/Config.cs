ab8bb30673d70dd91e0d606d4277161e9535e57c
ivanbenko <bivans209@gmail.com>
2016-04-29 12:14:57 +0200

    added FluentScheduler.Confifu

---
+++ src/FluentScheduler.Confifu/Config.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Confifu.Abstractions;
+
+namespace FluentScheduler.Confifu
+{
+    public static class Const
+    {
+        public const string Registry = "Registry";
+        public const string RegistryConfigFunc = "RegistryConfigFunc";
+        public const string Initialized = "Initialized";
+    }
+
+    public class Config
+    {
+        private readonly ConfigVariablesWrapper _vars;
+        private readonly IAppConfig _appConfig;
+        public static Config Current => new Config(App.Config);
+
+        public Config(IAppConfig appConfig)
+        {
+            if (appConfig == null) throw new ArgumentNullException(nameof(appConfig));
+            _appConfig = appConfig;
+            _vars = new ConfigVariablesWrapper(appConfig.GetConfigVariables(), "FluentScheduler:");
+        }
+
+        public bool Initialized
+        {
+            get { return _appConfig.Get<bool>(Const.Initialized); }
+            set { _appConfig[Const.Initialized] = value; }
+        }
+
+        public Func<Registry> Registry
+        {
+            get { return _appConfig[Const.Registry] as Func<Registry>; } 
+            set { _appConfig[Const.Registry] = value; }
+        }
+
+        public Action<Registry> RegistryConfigFunc
+        {
+            get { return _appConfig[Const.RegistryConfigFunc] as Action<Registry>; } 
+            set { _appConfig[Const.RegistryConfigFunc] = value; }
+        }
+    }
+
+    public static class AppConfigExtensions
+    {
+        public static IAppConfig InitFluentScheduler(this IAppConfig appConfig)
+        {
+            if (appConfig == null) throw new ArgumentNullException(nameof(appConfig));
+
+            return appConfig.ConfigureStrong(config =>
+            {
+                if (config.Initialized)
+                    return;
+
+                config.Initialized = true;
+
+                var lazyRegistry = new Lazy<GenericRegistry>(() =>
+                {
+                    var registry = new GenericRegistry();
+                    config.RegistryConfigFunc?.Invoke(registry);
+                    return registry;
+                });
+
+                config.Registry = () => lazyRegistry.Value;
+            })
+            .WrapAppRunner(runner => () =>
+            {
+                runner?.Invoke();
+                var config = appConfig.Strong();
+                JobManager.Initialize(config.Registry());
+            });
+        }
+
+        public static IAppConfig UseFluentScheduler(this IAppConfig appConfig, 
+            Action<Registry> configureFunc)
+        {
+            if (appConfig == null) throw new ArgumentNullException(nameof(appConfig));
+            if (configureFunc == null) throw new ArgumentNullException(nameof(configureFunc));
+
+            return appConfig
+                .InitFluentScheduler()
+                .ConfigureStrong(config =>
+            {
+                var currentConfigFunc = config.RegistryConfigFunc;
+                config.RegistryConfigFunc = registry =>
+                {
+                    currentConfigFunc?.Invoke(registry);
+                    configureFunc(registry);
+                };
+            });
+        }
+
+        public static IAppConfig ConfigureStrong(this IAppConfig appConfig, Action<Config> configFunc)
+        {
+            if (appConfig == null) throw new ArgumentNullException(nameof(appConfig));
+            if (configFunc == null) throw new ArgumentNullException(nameof(configFunc));
+
+            var config = appConfig.Strong();
+            configFunc(config);
+            return appConfig;
+        }
+
+        internal static Config Strong(this IAppConfig appConfig)
+        {
+            return new Config(appConfig);
+        }
+    }
+
+    internal class GenericRegistry : Registry
+    {
+    }
+}
---
+++ test/FluentScheduler.Confifu.Tests/ConfigTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Confifu;
+using Confifu.Abstractions;
+using FluentScheduler.Confifu;
+using FluentScheduler;
+
+namespace FluentScheduler.Confifu.Tests
+{
+    public class ConfigTests
+    {
+        [Fact]
+        public void it_does_not_smoke()
+        {
+            var appConfig = CreateAppConfig();
+            appConfig.UseFluentScheduler(r =>
+            {
+
+            });
+        }
+
+        [Fact]
+        public void it_schedules_and_run_task()
+        {
+            var itIsWorking = false;
+            var appConfig = CreateAppConfig();
+            appConfig.UseFluentScheduler(r =>
+            {
+                r.Schedule(() => {
+                    itIsWorking = true;
+                }).ToRunNow();
+            });
+            appConfig.GetAppRunner()?.Invoke();
+            
+            Assert.True(itIsWorking);
+        }
+        
+        private static AppConfig CreateAppConfig()
+        {
+            var appConfig = new AppConfig();
+            appConfig.SetConfigVariables(new DictionaryConfigVariables(new Dictionary<string, string>()));
+            return appConfig;
+        }
+
+    }
+}
