ccbb44c8fd620928bb69d648e532fe2db52c9b49
ivanbenko <bivans209@gmail.com>
2017-01-27 17:48:32 +0100

    moved back to .net core tools preview 2

---
+++ src/app/Confifu/ConfigVariables/LinkableConfigVariables.cs
@@ -0,0 +1,18 @@
+﻿using Confifu.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Confifu.ConfigVariables
+{
+    public class LinkableConfigVariables : IConfigVariables
+    {
+        public string this[string key] => null;
+
+        public LinkableConfigVariables(IConfigVariables underlying)
+        {
+            if (underlying == null)
+                throw new ArgumentNullException(nameof(underlying));
+        }
+    }
+}
---
+++ src/test/Confifu.Tests/ConfigVariables/LinkableConfigVariablesTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Confifu.Tests.ConfigVariables
+{
+    public class LinkableConfigVariablesTests
+    {
+        [Fact]
+        public void it_supports_links_inside_config_values()
+        {
+
+        }
+    }
+}
